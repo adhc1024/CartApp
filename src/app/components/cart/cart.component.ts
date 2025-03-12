a54b7c16d289f1588ed7d69b875fc8dbd2641542
Andres H <daandrewr10@gmail.com>
2025-03-11 21:34:36 -0500

    CartApp adicion de carrito de compras, catalogo y navbar,estilos basicos cargados para modal

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CartItem } from '../../models/cartItem';
+
+@Component({
+  selector: 'cart',
+  imports: [],
+  templateUrl: './cart.component.html'
+})
+export class CartComponent {
+
+  @Input() items : CartItem[] = [];
+  @Input() total = 0;
+  @Output() idProductEventEmitter = new EventEmitter();
+
+  onDeleteCart(id:number){
+    this.idProductEventEmitter.emit(id);
+  }
+}
---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products :Product[] = [
+    {
+        id: 1,
+        name: 'Phone XL',
+        price: 1000,
+        description: 'A large phone with one of the best screens'
+    },
+    {
+        id: 2,
+        name: 'Phone Mini',
+        price: 2000,
+        description: 'A great phone with one of the best cameras'
+    },
+    {
+        id: 3,
+        name: 'Phone Standard',
+        price: 3100,
+        description: ''
+    },
+    {
+        id: 4,
+        name: 'Tablet Pro',
+        price: 1800,
+        description: 'A powerful tablet with a stunning display'
+    },
+    {
+        id: 5,
+        name: 'Laptop Air',
+        price: 5000,
+        description: 'A lightweight laptop with great performance'
+    },
+    {
+        id: 6,
+        name: 'Smartwatch',
+        price: 4000,
+        description: 'A smartwatch with various health tracking features'
+    }
+];
